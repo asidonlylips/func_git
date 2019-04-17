677b189a74349b27d44117a3bad1c8cb2ddf12b2
asidonlylips <asidonlylips@sad.captain.github.com>
2019-04-17 18:08:11 +0300

    feature5

---
+++ feature.py
@@ -0,0 +1,9 @@
+def feature5(ins):
+    for i in range(len(ins)):
+        j = i - 1
+        temp = ins[i]
+        while ins[j] > temp and j >= 0:
+            ins[j + 1] = ins[j]
+            j -= 1
+        ins[j + 1] = temp
+    return ins
