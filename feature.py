97ce8db1a4665c4cf4d7e838175282aa997f595c
asidonlylips <asidonlylips@sad.captain.github.com>
2019-04-18 17:42:57 +0300

    refactor

--- feature.py
+++ feature.py
@@ -1,24 +1,13 @@
 import calendar
 
-def feature4_v1(year):
-    if len(str(year))==4:
-        if year % 4 != 0:
-            return "Обычный"
-        elif year % 100 == 0:
-            if year % 400 == 0:
-                return "Високосный"
-            else:
-                return "Обычный"
-        else:
-            return "Високосный"
-    else:
-        return "Число не четырехзначное"
 
-def feature4_v2(year):
-    if len(str(year)) == 4:
-        if calendar.isleap(year):
-            return "Високосный"
-        else:
-            return "Обычный"
-    else:
-        return "Число не четырехзначное"
+def is_leap_year_v1(year):
+    return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+
+
+def find_leap_year_v2(year):
+    result = False
+    if calendar.isleap(year):
+        result = True
+    return result
+
