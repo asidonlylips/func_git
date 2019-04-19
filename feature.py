c597f582610e3c00157613b7ebfa8d048b4fe6b1
asidonlylips <asidonlylips@sad.captain.github.com>
2019-04-19 14:19:18 +0300

    now working with files

--- feature.py
+++ feature.py
@@ -1,2 +1,9 @@
-def sort_by_lenth(args):
-   return sorted(args, key=len)
+from manager import Context_manager,write_to_file,read_file
+
+
+def sort_by_length(args):
+   return sorted(args.split(','), key=len)
+
+
+input_information = sort_by_length(read_file('1.txt'))
+write_to_file('2.txt', 'x', input_information)   
