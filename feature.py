856f180d90515c811ba2ab3bfadfddccf9909959
asidonlylips <asidonlylips@users.noreply.github.com>
2019-04-21 19:13:49 +0300

    rename function, changed second function

--- feature.py
+++ feature.py
@@ -1,13 +1,9 @@
-import calendar
+from calendar import isleap
 
 
 def is_leap_year_v1(year):
     return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
 
 
-def find_leap_year_v2(year):
-    result = False
-    if calendar.isleap(year):
-        result = True
-    return result
-
+def  is_leap_year_v2(year):
+    return True if isleap(year) else False
