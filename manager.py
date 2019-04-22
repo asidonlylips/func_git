365378c676a7327f5347c87e503f6fc72b36684d
asidonlylips <49650592+asidonlylips@users.noreply.github.com>
2019-04-22 16:28:56 +0300

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,19 +1,22 @@
 class Context_manager():
+
     def __init__(self, file_name, method):
         self.file_name = file_name
         self.method = method
+
     def __enter__(self):
-        self.file = open(self.file_name, self.method) 
+        self.file = open(self.file_name, self.method)
         return self.file
+
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.file.close()
 
 
 def read_file(file_name):
-    with Context_manager(file_name,'r') as f_read: 
+    with Context_manager(file_name, 'r') as f_read:
         return f_read.read()
 
 
-def write_to_file(file_name,method,your_text):
-    with Context_manager(file_name,method) as f_write:
+def write_to_file(file_name, method, your_text):
+    with Context_manager(file_name, method) as f_write:
         f_write.write('\n' + your_text)
