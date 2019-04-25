1728822a624a62bef6c76168cd6aba073475a963
asidonlylips <asidonlylips@sad.captain.github.com>
2019-04-25 14:48:39 +0300

    Remastered

--- builder.py
+++ builder.py
@@ -1,134 +1,56 @@
-class Control:
-
+class Vehicle:
     def __init__(self):
-        self._builder = None
+        self.wheel = None
+        self.engine = None
+        self.transmission = None
+    
+    def __str__(self):
+        result = "Engine: {}, Number of wheels: {}, Transmission: {}".format(self.vehicle.engine, self.vehicle.wheel, self.vehicle. transmission)
+        return result
 
-    def construct(self, builder):
-        self.__builder = builder
-
-    def getcar(self):
-        car = Car()
-        name = self.__builder.getname()
-        car.set_name(name)
-        wheel = self.__builder.getwheel()
-        car.set_wheel(wheel)
-        engine = self.__builder.getengine()
-        car.set_engine(engine)
-        transmission = self.__builder.gettransmission()
-        car.set_transmission(transmission)
-        return car
-
-
-class Car:
+class Vehiclebuilder(Vehicle):
     def __init__(self):
-        self.__name = None
-        self.__wheel = None
-        self.__engine = None
-        self.__transmission = None
+        self.vehicle = Vehicle()
+    
+    def add_wheels_car(self):
+        self.vehicle.wheel = 4
+        return self
+    
+    def add_wheels_motorcycle(self):
+        self.vehicle.wheel = 2
+        return self
 
-    def set_name(self, name):
-        self.__name = name
+    def add_engine_car(self):        
+        self.vehicle.engine = "V8"
+        return self
 
-    def set_wheel(self, numbers):
-        self.__wheel = numbers
+    def add_engine_motorcycle(self):
+        self.vehicle.engine = "V4"
+        return self
+    
+    def add_auto_transmission(self):
+        self.vehicle.transmission = 'auto'
+        return self
 
-    def set_engine(self, engine):
-        self.__engine = engine
-
-    def set_transmission(self, transmission):
-        self.__transmission = transmission
-
-    def carinfo(self):
-        print('Vehicle name: {}'.format(self.__name))
-        print('{} engine with {} horsepower'.format(self.__engine.type_engine, self.__engine.horsepower))
-        print('Number of wheels: {}'.format(self.__wheel))
-        print('Transmission type: {}'.format(self.__transmission))
-
-
-class Builder:
-    def getname(self): pass
-
-    def getengine(self): pass
-
-    def gettransmission(self): pass
-
-    def getwheel(self): pass
-
-
-class Name:
-    name = None
-
-
-class Wheel:
-    number = None
-
-
-class Engine:
-    type_engine = None
-    horsepower = None
-
-
-class Transmission:
-    transmission_type = None
-
-
-class MuscleCar(Builder):
-    def getname(self):
-        name = Name()
-        name = 'DIABLO'
-        return name
-
-    def getengine(self):
-        engine = Engine()
-        engine.type_engine = 'V8'
-        engine.horsepower = 800
-        return engine
-
-    def getwheel(self):
-        wheel = Wheel()
-        wheel = 4
-        return wheel
-
-    def gettransmission(self):
-        transmission = Transmission()
-        transmission = 'auto'
-        return transmission
-
-
-class Motorcycle(Builder):
-    def getname(self):
-        name = Name()
-        name = 'Hellish chariot'
-        return name
-
-    def getengine(self):
-        engine = Engine()
-        engine.type_engine = 'V4'
-        engine.horsepower = 300
-        return engine
-
-    def getwheel(self):
-        wheel = Wheel()
-        wheel = 2
-        return wheel
-
-    def gettransmission(self):
-        transmission = Transmission()
-        transmission = 'manual'
-        return transmission
-
-
-def main():
-    carbuilder = MuscleCar()
-    control = Control()
-    control.construct(carbuilder)
-    my_car = control.getcar()
-    my_car.carinfo()
-    motobuilder = Motorcycle()
-    control.construct(motobuilder)
-    my_moto = control.getcar()
-    my_moto.carinfo()
+    def add_manual_transmission(self):
+        self.vehicle.transmission = 'manual'
+        return self
+    
+    def build(self):
+        return self.vehicle
 
 
 if __name__ == "__main__":
-    main()
+    my_car = Vehiclebuilder()
+    my_car.add_auto_transmission()
+    my_car.add_engine_car()
+    my_car.add_wheels_car()
+    my_car.build()
+    print(my_car)
+    my_motorcycle = Vehiclebuilder()
+    my_motorcycle.add_manual_transmission()
+    my_motorcycle.add_engine_motorcycle()
+    my_motorcycle.add_wheels_motorcycle()
+    my_motorcycle.build()
+    print(my_motorcycle)
+    
